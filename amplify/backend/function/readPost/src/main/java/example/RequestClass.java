7346a2734700240a2b7ef521aba5a6f6ba19220b
AKS-V02 <a.k.s02@outlook.com>
2023-02-19 20:14:42 +0530

    test environment

---
+++ amplify/backend/function/readPost/src/main/java/example/RequestClass.java
@@ -0,0 +1,38 @@
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+	STORAGE_POSTTABLE_ARN
+	STORAGE_POSTTABLE_NAME
+	STORAGE_POSTTABLE_STREAMARN
+Amplify Params - DO NOT EDIT */
+
+package example;
+        
+     public class RequestClass {
+        String firstName;
+        String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        public RequestClass(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public RequestClass() {
+        }
+    }
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,45 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "post": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "newapp100cd987": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "readPost": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "PostPhotoStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    },
+    "PostTable": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
