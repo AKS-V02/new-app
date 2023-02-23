a50c639e75c1c650f5c8f9ffd2bfd2b73d5adaed
AKS-V02 <a.k.s02@outlook.com>
2023-02-23 17:18:37 +0530

    env variable added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function onclickfunction(){
+
+    alert(`environmet is ${process.env.USER_BRANCH}`);
+  }
+
+
   return (
     <div className="App">
       new App
+      <p>{process.env.USER_BRANCH}</p>
+      <button
+            type="button"
+            className=""
+            onClick={onclickfunction}>
+                Check Environment
+            </button>
     </div>
   );
 }
