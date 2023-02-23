605ce806416d8edbe05a5be841b2c791c0a94a70
AKS-V02 <a.k.s02@outlook.com>
2023-02-23 21:19:19 +0530

    env variable added .env file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { envVar } from './envVar';
 
 function App() {
-  const abs = process.env.REACT_APP_USER_BRANCH;
+  const abs = envVar[process.env.REACT_APP_Environment];
 
   function onclickfunction(){
 
-    alert("environmet is "+abs);
+    alert("environmet is "+process.env.REACT_APP_Environment);
   }
 
 
   return (
     <div className="App">
       new App
-      <p>{process.env.REACT_APP_COMPANY}</p>
+      <p>{abs.somting}</p>
+      <p>{abs.dothing}</p>
       <button
             type="button"
             className=""
